a8cf4694d85a757147e8cdbab633ec24fc3da3c6
Mazaj-ru <mazaj.ru@mail.ru>
2022-11-12 19:27:41 +0500

    create block header

--- js/script.js
+++ js/script.js
@@ -1,29 +1,27 @@
 // Мобильное меню
-// const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector('.hamburger');
 // const mobNav = document.querySelector('.nav');
-// const overlay = document.querySelector('#overlay');
-// const noScroll = document.body;
+const overlay = document.querySelector('#overlay');
+const noScroll = document.body;
 
-// hamburger.addEventListener('click', function () {
-
-//     this.classList.toggle("active");
-//     mobNav.classList.toggle("active");
-//     overlay.classList.toggle("active");
-//     noScroll.classList.toggle("noscroll");
-// });
+hamburger.addEventListener('click', function () {
+    this.classList.toggle("active");
+    // mobNav.classList.toggle("active");
+    // overlay.classList.toggle("active");
+    // noScroll.classList.toggle("noscroll");
+});
 
 // mobNav.addEventListener('click', function() {
-
 //     this.classList.remove('active');
 //     overlay.classList.remove("active");
 //     noScroll.classList.remove("noscroll");
 //     hamburger.classList.remove("active");
 // });
 
-// // Закрытие мобильного меню при ресайзе экрана
-// window.addEventListener('resize', function(){
-//     mobNav.classList.remove('active');
-//     overlay.classList.remove("active");
-//     noScroll.classList.remove("noscroll");
-//     hamburger.classList.remove("active");
-// });
+// Закрытие мобильного меню при ресайзе экрана
+window.addEventListener('resize', function(){
+    // mobNav.classList.remove('active');
+    // overlay.classList.remove("active");
+    // noScroll.classList.remove("noscroll");
+    hamburger.classList.remove("active");
+});
