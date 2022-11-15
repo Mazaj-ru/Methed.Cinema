29a1ecc1f847dc1db75c57ff5dc47b273e8669a2
Mazaj-ru <mazaj.ru@mail.ru>
2022-11-15 20:16:15 +0500

    Доработал меню

--- js/script.js
+++ js/script.js
@@ -5,20 +5,25 @@ const overlay = document.querySelector('#overlay');
 const noScroll = document.body;
 
 hamburger.addEventListener('click', function () {
+    if (noScroll.classList.contains('noscroll')) {
+      noScroll.style.cssText = '';
+    } else {
+      noScroll.style.cssText = `padding-right: ${window.innerWidth - noScroll.offsetWidth}px`;
+    }
     this.classList.toggle("active");
     mobNav.classList.toggle("active");
-    // noScroll.classList.toggle("noscroll");
+    noScroll.classList.toggle("noscroll");
 });
 
 mobNav.addEventListener('click', function() {
     this.classList.remove('active');
-    // noScroll.classList.remove("noscroll");
+    noScroll.classList.remove("noscroll");
     hamburger.classList.remove("active");
 });
 
 // Закрытие мобильного меню при ресайзе экрана
 window.addEventListener('resize', function(){
     mobNav.classList.remove('active');
-    // noScroll.classList.remove("noscroll");
+    noScroll.classList.remove("noscroll");
     hamburger.classList.remove("active");
 });
